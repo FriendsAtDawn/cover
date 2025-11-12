096b94ccc5dc6e6b344dd72f3ce983fab1ecc7d9
Stand Your Gists <76827290+FriendsAtDawn@users.noreply.github.com>
2025-11-11 23:21:43 -0300

    Refactor HTML meta tags in portis.sh

--- portis.sh
+++ portis.sh
@@ -23,11 +23,10 @@ echo "<!DOCTYPE html>
 <html>
 <head>
  <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">
- <meta charset="UTF-8">
- <meta name="description" content="Portal de Buscador de Jurisprudencia, blog, recursos, RUC etc.">
-  <meta name="keywords" content="jurisprudencia, buscador de jurisprudencia, buscador avanzado, portal">
-  <meta name="author" content="Wilson Villalba">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <meta name=\"description\" content=\"Portal de Buscador de Jurisprudencia, blog, recursos, RUC etc.\">
+  <meta name=\"keywords\" content=\"jurisprudencia, buscador de jurisprudencia, buscador avanzado, portal\">
+  <meta name=\"author\" content=\"Wilson Villalba\">
+  <meta name="viewport" content=\"width=device-width, initial-scale=1.0\">
  <title>villalba.is.eu.org</title>
  <link rel=\"stylesheet\" href=\"./styles/lit.css\" />
  <script src=\"./js/rss-feed.js\" defer></script>
